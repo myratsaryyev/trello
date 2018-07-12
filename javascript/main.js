4f8495941c21c6d7766d8a48c16acaa747683add
myrat <myrat@MacBook-Pro-myrat.local>
2018-07-12 20:45:07 +0300

    Made some chenges

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+//privedeniya tipov
+//LexicalEnvironment ,   Scoup, [[Scoup]], class,?
+
+// var f = function(x){
+//     return function(y){
+//         return x + y;
+//     };
+// };
+// a = f(5);
+// console.log(a);
+// // sama vyzyvayushe funksi
+// var a = 5;
+// (function(){
+//     alert(a);
+// })()
+
+
+
